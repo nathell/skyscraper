ddd7a12cffed03e06915b51abbe50989c60f914a
Daniel Janus <dj@danieljanus.pl>
2016-05-06 14:14:55 +0200

    Bump Clojure version to 1.8.0.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Structural scraping for the rest of us."
   :license {:name "MIT", :url "https://github.com/nathell/skyscraper/blob/master/README.md#license"}
   :scm {:name "git", :url "https://github.com/nathell/skyscraper"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.3"]
                  [clj-http "2.0.0"]
                  [crouton "0.1.2"]
