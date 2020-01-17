b5f1aca6186e64284686f7ef09863809b2d39aca
Daniel Janus <dj@danieljanus.pl>
2020-01-17 20:05:07 +0100

    Add docstrings and better names in skyscraper.db.

--- src/skyscraper/db.clj
+++ src/skyscraper/db.clj
@@ -9,19 +9,29 @@
     [skyscraper.data :refer [separate]]
     [taoensso.timbre :refer [debugf warnf]]))
 
-(defn- db-name [col]
-  (string/replace (name col) "-" "_"))
+(defn- keyword->db-name
+  "Converts a keyword (naming a DB table or column) to a string
+  suitable for use in SQL queries."
+  [k]
+  (string/replace (name k) "-" "_"))
 
-(defn- create-index-ddl [table-name key-column-names]
+(defn- db-name->keyword
+  "The inverse of keyword->db-name."
+  [str]
+  (keyword (string/replace str "_" "-")))
+
+(defn- create-index-ddl
+  "Returns SQL to create an index on a given table and columns. Note that
+  Skyscraper only creates one index per table, so it's sufficient to just
+  name it after the table."
+  [table-name key-column-names]
   (let [index-name (str "idx_" table-name)]
     (str "CREATE UNIQUE INDEX " index-name " ON " table-name " (" (string/join ", " key-column-names) ")")))
 
-(defn- normalize-keys [m]
-  (into {}
-        (map (fn [[k v]] [(keyword (string/replace (name k) "_" "-")) v]))
-        m))
-
-(defn- create-table [db table-name column-names key-column-names]
+(defn- create-table
+  "Creates a table in db containing the given column-names. If key-column-names
+  is non-empty, also creates a unique index on those columns."
+  [db table-name column-names key-column-names]
   (jdbc/execute! db
                  (jdbc/create-table-ddl
                   table-name
@@ -33,17 +43,21 @@
     (jdbc/execute! db
                    (create-index-ddl table-name key-column-names))))
 
-(defn- query [db table-name id ctxs]
-  (let [id-part (string/join ", " (map db-name id))
-        values-1 (str "(" (string/join ", " (repeat (count id) "?")) ")")
+(defn- query-context-ids
+  "Selects the rows corresponding to the upserted contexts, to retrieve
+  their database-assigned IDs."
+  [db table-name key-columns key-column-names ctxs]
+  (let [key-part (string/join ", " key-column-names)
+        values-1 (str "(" (string/join ", " (repeat (count key-column-names) "?")) ")")
         values (string/join ", " (repeat (count ctxs) values-1))
-        null-clause (string/join " or " (map #(str (db-name %) " is null") id)) ;; XXX: this might return too broad a result set
-        query (<< "select * from ~{table-name} where (~{id-part}) in (values~{values}) or ~{null-clause}")
-        params (mapcat (apply juxt id) ctxs)]
-    (map normalize-keys
-         (jdbc/query db (into [query] params)))))
+        null-clause (string/join " or " (map #(str % " is null") key-column-names)) ;; XXX: this might return too broad a result set
+        query (<< "select * from ~{table-name} where (~{key-part}) in (values~{values}) or ~{null-clause}") ;; XXX: only select id + key columns, not *
+        params (mapcat (apply juxt key-columns) ctxs)]
+    (jdbc/query db (into [query] params)
+                {:identifiers db-name->keyword})))
 
 (defn- upsert-multi-row-sql
+  "Returns SQL for upsert-multi!"
   [table-name column-names key-column-names values]
   (let [nc (count column-names)
         vcs (map count values)
@@ -58,12 +72,23 @@
                       (<< " ON CONFLICT (~(comma-join key-column-names)) DO UPDATE SET ~{set-clause}"))))]
             values))))
 
-(defn- upsert-multi! [db table-name column-names key-column-names rows]
+(defn- upsert-multi!
+  "Like clojure.java.jdbc/insert-multi!, but updates the existing rows
+  where key-column-names match supplied ones. Requires rows to be a
+  sequence of vectors. Not wrapped in a transaction.
+  Equivalent to insert-multi! if key-column-names is empty.
+  Note: This is currently implemented as an INSERT ... ON CONFLICT DO
+  UPDATE, which requires a DBMS able to support this syntax (SQLite
+  3.24+ or PostgreSQL 9.5+)."
+  [db table-name column-names key-column-names rows]
   (jdbc/db-do-prepared db false
                        (upsert-multi-row-sql table-name column-names key-column-names rows)
                        {:multi? true}))
 
-(defn- upsert-multi-ensure-table! [db table-name column-names key-column-names rows]
+(defn- upsert-multi-ensure-table!
+  "Tries an upsert-multi!, and if it fails due to a missing table,
+  creates it and tries again."
+  [db table-name column-names key-column-names rows]
   (try
     (upsert-multi! db table-name column-names key-column-names rows)
     (catch org.sqlite.SQLiteException e
@@ -73,7 +98,11 @@
           (upsert-multi! db table-name column-names key-column-names rows))
         (throw e)))))
 
-(defn- ensure-types-single [columns context]
+(defn- ensure-types-single
+  "Returns context, emitting warnings if the fields named by columns
+  don't exist or are not of expected type (int for :parent, nilable string
+  otherwise)."
+  [columns context]
   (doseq [[k v] context
           :when (contains? columns k)
           :let [check (if (= k :parent) int? #(or (nil? %) (string? %)))]
@@ -86,11 +115,19 @@
   (merge (zipmap columns (repeat nil))
          context))
 
-(defn- ensure-types [columns ctxs]
+(defn- ensure-types
+  "Ensures types of all contexts as per ensure-types-single."
+  [columns ctxs]
   (mapv (partial ensure-types-single columns) ctxs))
 
-(defn- extract-ids [db table-name key-columns ctxs]
-  (let [inserted-rows (query db table-name key-columns ctxs)
+(defn- extract-ids
+  "Given a sequence of ctxs that are assumed to exist in the given db table,
+  queries the DB for them and assocs each one's id as :parent."
+  ;; Remember that this runs after the processor's :process-fn, so
+  ;; calling it :parent ensures that the child processors will encounter
+  ;; this in the expected place.
+  [db table-name key-columns key-column-names ctxs]
+  (let [inserted-rows (query-context-ids db table-name key-columns key-column-names ctxs)
         inserted-row-ids (into {}
                                (map (fn [r] [(select-keys r key-columns) (:id r)]))
                                inserted-rows)]
@@ -98,37 +135,46 @@
            (assoc ctx :parent (get inserted-row-ids (select-keys ctx key-columns))))
          ctxs)))
 
-(defn- extract-ids-from-last-rowid [db ctxs]
+(defn- extract-ids-from-last-rowid
+  "Given a sequence of ctxs that have just been successfully inserted,
+  assocs each one's id in the DB as :parent based on last_insert_rowid()
+  (SQLite-specific)."
+  [db ctxs]
   (let [rowid (-> (jdbc/query db "select last_insert_rowid() rowid") first :rowid)]
     (map #(assoc %1 :parent %2) ctxs (range (inc (- rowid (count ctxs))) (inc rowid)))))
 
-(defn insert-all!
+(defn upsert-contexts
   "Inserts new contexts into a given db table, returning them augmented
-  with the `:id` fields corresponding to the DB-generated primary
+  with the `:parent` fields corresponding to the DB-generated primary
   keys.  If `key-columns` (a vector of column names) is provided,
-  checks for the presence of matching contexts in DB and only inserts
-  those that were not present."
+  does an upsert rather than an insert, checking for conflicts on
+  those columns and updating db accordingly."
   [db table key-columns columns ctxs]
   (debugf "Upserting %s rows" (count ctxs))
   (let [ctxs (ensure-types (set columns) ctxs)
-        table-name (db-name table)
-        column-names (mapv db-name columns)
-        key-column-names (mapv db-name key-columns)
+        table-name (keyword->db-name table)
+        column-names (mapv keyword->db-name columns)
+        key-column-names (mapv keyword->db-name key-columns)
         rows (map (apply juxt columns) ctxs)]
     (upsert-multi-ensure-table! db table-name column-names key-column-names rows)
     (if (seq key-column-names)
-      (extract-ids db table-name key-columns ctxs)
+      (extract-ids db table-name key-columns key-column-names ctxs)
       (extract-ids-from-last-rowid db ctxs))))
 
-(defn maybe-store-in-db [db {:keys [name db-columns id] :as q} contexts]
+(defn maybe-store-in-db
+  "Wraps upsert-context, skipping contexts that contain :skyscraper.core/db-skip."
+  [db {:keys [name db-columns id] :as processor} contexts]
   (if (and db db-columns)
     (let [db-columns (distinct (conj db-columns :parent))
           [skipped inserted] (separate :skyscraper.core/db-skip contexts)
-          new-items (insert-all! db name id db-columns inserted)]
+          new-items (upsert-contexts db name id db-columns inserted)]
       (into (vec skipped) new-items))
     contexts))
 
-(defn enhancer [{:keys [db]} {:keys [enhancer-input-chan enhancer-output-chan]}]
+(defn enhancer
+  "An enhancer that upserts supplied batches of contexts into
+  the database."
+  [{:keys [db]} {:keys [enhancer-input-chan enhancer-output-chan]}]
   (jdbc/with-db-transaction [db db]
     (loop []
       (when-let [item (async/<!! enhancer-input-chan)]
--- test/skyscraper/db_test.clj
+++ test/skyscraper/db_test.clj
@@ -11,24 +11,24 @@
         update [{:name "John", :surname "Doe", :phone "765-43-21"}]]
     (testing "basic usecase"
       (with-temporary-sqlite-db db
-        (db/insert-all! db :example-data nil [:name :surname :phone] example-data)
+        (db/upsert-contexts db :example-data nil [:name :surname :phone] example-data)
         (is (= (jdbc/query db "SELECT name, surname, phone FROM example_data ORDER BY id")
                example-data))))
     (testing "multiple inserts duplicate data"
       (with-temporary-sqlite-db db
         (dotimes [_ 5]
-          (db/insert-all! db :example-data nil [:name :surname :phone] example-data))
+          (db/upsert-contexts db :example-data nil [:name :surname :phone] example-data))
         (is (= (jdbc/query db "SELECT count(*) cnt FROM example_data")
                [{:cnt 10}])))
       (testing "unless key is supplied"
         (with-temporary-sqlite-db db
           (dotimes [_ 5]
-            (db/insert-all! db :example-data [:name :surname] [:name :surname :phone] example-data))
+            (db/upsert-contexts db :example-data [:name :surname] [:name :surname :phone] example-data))
           (is (= (jdbc/query db "SELECT count(*) cnt FROM example_data")
                  [{:cnt 2}])))))
     (testing "updates"
       (with-temporary-sqlite-db db
         (doseq [data [example-data update]]
-          (db/insert-all! db :example-data [:name :surname] [:name :surname :phone] data))
+          (db/upsert-contexts db :example-data [:name :surname] [:name :surname :phone] data))
         (is (= (jdbc/query db "SELECT name, surname, phone FROM example_data ORDER BY id")
                [(first update) (second example-data)]))))))
