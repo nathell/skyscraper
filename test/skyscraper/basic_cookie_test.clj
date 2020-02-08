2f97438b65207c9296ff52b02fd1525121a2392d
Daniel Janus <dj@danieljanus.pl>
2020-02-08 19:45:21 +0100

    Modularize tests: basic-cookie-test.

--- test/skyscraper/basic_cookie_test.clj
+++ test/skyscraper/basic_cookie_test.clj
@@ -1,22 +1,16 @@
-(ns skyscraper.real-test
+(ns skyscraper.basic-cookie-test
   (:require
-    [clojure.java.jdbc :as jdbc]
-    [clojure.string :as string]
     [clojure.test :as test :refer [deftest is]]
     [hiccup.page :refer [html5]]
     [net.cgrand.enlive-html :refer [select text]]
-    [ring.adapter.jetty :refer [run-jetty]]
     [ring.middleware.cookies :refer [wrap-cookies]]
-    [ring.util.response :as response]
     [skyscraper.cache :as cache]
-    [skyscraper.core :refer :all]
+    [skyscraper.core :refer [defprocessor scrape scrape!]]
     [skyscraper.enlive-helpers :refer [href]]
-    [skyscraper.test-utils :refer [make-seed resp-page with-server with-temporary-sqlite-db]]
+    [skyscraper.test-utils :refer [make-seed resp-page with-server]]
     [taoensso.timbre :as timbre]))
 
-;; basic-cookie-test
-
-(defn basic-cookie-test-handler [{:keys [cookies uri]}]
+(defn handler [{:keys [cookies uri]}]
   (condp = uri
     "/" {:status 200,
          :headers {"Set-Cookie" "secret=donttellanyone"},
@@ -27,19 +21,19 @@
                      "You ain't got it")])
     {:status 404}))
 
-(defprocessor :basic-cookie-test-root
+(defprocessor ::root
   :process-fn (fn [res ctx]
                 (for [link (select res [:a])]
-                  {:link-text (text link), :url (href link), :processor :basic-cookie-test-secret})))
+                  {:link-text (text link), :url (href link), :processor ::secret})))
 
-(defprocessor :basic-cookie-test-secret
+(defprocessor ::secret
   :process-fn (fn [res ctx]
                 (for [item (select res [:p])]
                   {:target (text item)})))
 
 (deftest basic-cookie-test
   (timbre/set-level! :warn)
-  (let [seed (make-seed :basic-cookie-test-root)]
-    (with-server (wrap-cookies basic-cookie-test-handler)
+  (let [seed (make-seed ::root)]
+    (with-server (wrap-cookies handler)
       (is (= (scrape seed)
              [{:link-text "Got a cookie?", :target "You got it!"}])))))
