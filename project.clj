17e9bd1b594deb0d9c02d8737f59e091c8c94837
Daniel Janus <dj@danieljanus.pl>
2022-07-22 21:15:53 +0200

    Update dependencies to latest
    All hail `lein ancient`!

--- project.clj
+++ project.clj
@@ -4,17 +4,17 @@
   :scm {:name "git", :url "https://github.com/nathell/skyscraper"}
   :codox {:metadata {:doc/format :markdown}}
   :url "https://github.com/nathell/skyscraper"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "1.0.567"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/core.async "1.5.648"]
                  [org.clojure/core.incubator "0.1.4"]
-                 [org.clojure/data.csv "1.0.0"]
-                 [org.clojure/data.priority-map "1.0.0"]
-                 [org.clojure/java.jdbc "0.7.11"]
-                 [clj-http "3.10.0"]
+                 [org.clojure/data.csv "1.0.1"]
+                 [org.clojure/data.priority-map "1.1.0"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [clj-http "3.12.3"]
                  [crouton "0.1.2"]
                  [enlive "1.1.6" :exclusions [jsoup]]
                  [reaver "0.1.3"]
-                 [com.taoensso/timbre "4.10.0"]
-                 [org.xerial/sqlite-jdbc "3.30.1"]]
+                 [com.taoensso/timbre "5.2.1"]
+                 [org.xerial/sqlite-jdbc "3.36.0.3"]]
   :profiles {:test {:dependencies [[hiccup "1.0.5"]
-                                   [ring "1.8.0"]]}})
+                                   [ring "1.9.5"]]}})
