475e44350cbaed4fa2f22adebdaf27f505da1c56
Daniel Janus <dj@danieljanus.pl>
2022-09-01 20:48:48 +0200

    Use lein-tools-deps in lieu of dependencies in project.clj
    To avoid duplication while still being able to build a jar /
    deploy to Clojars with Leiningen. Eventually we'll migrate to
    cli-tools completely, but not yet.

--- project.clj
+++ project.clj
@@ -4,18 +4,6 @@
   :scm {:name "git", :url "https://github.com/nathell/skyscraper"}
   :codox {:metadata {:doc/format :markdown}}
   :url "https://github.com/nathell/skyscraper"
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/core.async "1.5.648"]
-                 [org.clojure/core.incubator "0.1.4"]
-                 [org.clojure/data.csv "1.0.1"]
-                 [org.clojure/data.priority-map "1.1.0"]
-                 [org.clojure/java.jdbc "0.7.12"]
-                 [clj-http "3.12.3"]
-                 [crouton "0.1.2"]
-                 [enlive "1.1.6" :exclusions [jsoup]]
-                 [reaver "0.1.3"]
-                 [com.taoensso/timbre "5.2.1"]
-                 [org.xerial/sqlite-jdbc "3.36.0.3"]]
-  :profiles {:test {:dependencies [[hiccup "1.0.5"]
-                                   [ring "1.9.5"]
-                                   [tortue/spy "2.13.0"]]}})
+  :plugins [[lein-tools-deps "0.4.5"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {})
